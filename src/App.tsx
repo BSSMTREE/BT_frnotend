3a1e9344551a5bc27939ba7f1b632cbfefbc0d56
jyh071116 <jyh071116@gmail.com>
2023-12-23 21:40:16 +0900

    feat: 텍스트 추가

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,20 @@ function App() {
   return (
     <S.Layout>
       <S.Header>BSSMTREE</S.Header>
+      <S.TextLayout>
+        <S.MainText>
+          부산소프트웨어마이스터고의 트리에요
+          <br />
+          현재 <S.Count>0</S.Count>개의 편지가 쌓여있어요
+          <br />
+          <br />
+          <S.SubText>
+            트리를 눌러 편지를 써주세요!
+            <br />
+            트리 속 편지는 12월 26일에 공개돼요.
+          </S.SubText>
+        </S.MainText>
+      </S.TextLayout>
     </S.Layout>
   );
 }
--- src/style.ts
+++ src/style.ts
@@ -19,3 +19,20 @@ export const Header = styled.div`
   border-radius: 0 0 20px 20px;
   box-shadow: 0px 2px 3px rgba(0, 0, 0, 0.25);
 `;
+
+export const TextLayout = styled.div`
+  padding: 20px 15px;
+`;
+export const Count = styled.span`
+  font-weight: 700;
+`;
+
+export const MainText = styled.div`
+  font-size: 18px;
+  font-weight: 600;
+`;
+
+export const SubText = styled.div`
+  font-size: 10px;
+  font-weight: 400;
+`
