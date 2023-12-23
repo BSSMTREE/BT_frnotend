8224c3d5f96db762d691981c641da8b9bf07cf06
jyh071116 <jyh071116@gmail.com>
2023-12-24 01:10:47 +0900

    feat: letter api 연동

--- src/components/Write/index.tsx
+++ src/components/Write/index.tsx
@@ -1,26 +1,53 @@
+import { useState } from "react";
 import * as S from "./style";
 import arrow from "assets/rightArrow1.svg";
+import { toast } from "react-toastify";
+import { letter } from "apis/letter";
 
 interface IWriteProps {
   setIsOpenWrite: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
 function Write({ setIsOpenWrite }: IWriteProps) {
+  const [writer, setWriter] = useState("");
+  const [recipient, setRecipient] = useState("");
+  const [detail, setDetail] = useState("");
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (writer === "" || recipient === "" || detail === "")
+        return toast.error("모든 필수 내용들을 입력해주세요!")
+    try {
+      await letter(writer, recipient, detail);
+      toast.success("성공적으로 편지를 전송했어요!");
+    } catch {
+      toast.error("편지를 발송하지 못하였습니다!");
+    }
+  };
   return (
-    <S.Write>
+    <S.Write onSubmit={handleSubmit}>
       <S.NameLayout>
         <div>
           <S.ToFrom>From.</S.ToFrom>
-          <S.Name placeholder="닉네임"></S.Name>
+          <S.Name
+            placeholder="닉네임"
+            onChange={(e) => setWriter(e.currentTarget.value)}
+          ></S.Name>
         </div>
         <S.Arrow src={arrow} />
         <div>
           <S.ToFrom>To.</S.ToFrom>
-          <S.Name placeholder="받는사람"></S.Name>
+          <S.Name
+            placeholder="받는사람"
+            onChange={(e) => setRecipient(e.currentTarget.value)}
+          ></S.Name>
         </div>
       </S.NameLayout>
 
-      <S.Message placeholder="따듯한 메세지를 적어주세요." />
+      <S.Message
+        placeholder="따듯한 메세지를 적어주세요."
+        onChange={(e) => setDetail(e.currentTarget.value)}
+      />
       <S.ButtonLayout>
         <S.Cancel type="button" onClick={() => setIsOpenWrite(false)}>
           이전
