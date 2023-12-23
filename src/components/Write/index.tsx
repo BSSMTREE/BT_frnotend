6d909e39de6a4413e3928245d3c5ef81b93ffdb7
jyh071116 <jyh071116@gmail.com>
2023-12-23 22:23:58 +0900

    feat: Write 페이지 퍼블리싱

--- src/components/Write/index.tsx
+++ src/components/Write/index.tsx
@@ -2,8 +2,14 @@ import * as S from "./style";
 
 function Write() {
   return (
-    <S.Write></S.Write>
-  )
+    <S.Write>
+      <S.ToFrom>To.</S.ToFrom>
+      <S.Name placeholder="보낼 사람(최대 8글자)"></S.Name>
+      <S.ToFrom>From.</S.ToFrom>
+      <S.Name placeholder="닉네임(최대 8글자)"></S.Name>
+      <S.Message placeholder="따듯한 메세지를 적어주세요." />
+    </S.Write>
+  );
 }
 
 export default Write;
--- src/components/Write/style.ts
+++ src/components/Write/style.ts
@@ -3,15 +3,46 @@ import writeBackground from "assets/writeBackground.svg";
 
 export const Write = styled.form`
   position: fixed;
+  width: 70%;
+  aspect-ratio: 2/3;
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
-  width: 70%;
-  aspect-ratio: 2/3;
   background-image: url(${writeBackground});
   background-repeat: no-repeat;
   background-size: cover;
   background-position: center;
   border-radius: 10px;
   filter: blur(0px);
+  padding: 30px;
 `;
+
+export const ToFrom = styled.div`
+  font-size: 0.7rem;
+  color: rgba(255, 255, 255, 0.75);
+  margin-bottom: 1px;
+`;
+
+export const Name = styled.input`
+  font-size: 0.8rem;
+  color: white;
+  background: transparent;
+  border: none;
+  outline: none;
+  margin-bottom: 7.5px;
+  &::placeholder {
+    color: rgba(255, 255, 255, 0.75);
+  }
+`
+
+export const Message = styled.input`
+  font-size: 0.6rem;
+  color: white;
+  background: transparent;
+  border: none;
+  outline: none;
+  margin-top: 10px;
+  &::placeholder {
+    color: rgba(255, 255, 255, 0.75);
+  }
+`
